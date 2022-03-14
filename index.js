730a33703865ae6e222e3b69ab3bd462e737385f
AngelSM11 <82569640+AngelSM11@users.noreply.github.com>
2022-03-14 19:30:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const cool = require("cool-ascii-faces");
-const express=require("express");
-console.log(cool())
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use("/",express.static('public'));
-
-app.get("/cool",(req,res)=>{
-    console.log("request /faces route");
-    res.send("<html><body><h1>"+cool()+"<h1></h1></body></html>"); 
+app.get("/",(req,res)=>{
+    console.log("request /route");
+    res.send("<html><body><h1>"+cool()+"<h1></h1></body></html>");
 });
 
-app.get("/time",(req,res)=>{
-    console.log("request /time route");
-    res.send("<html><body><h1>"+new Date()+"<h1></h1></body></html>");
-});
 
 
 app.listen(port, () =>{
-    console.log('Server ready at port {$port}');
+    console.log('Server ready at port ${port}');
 });
