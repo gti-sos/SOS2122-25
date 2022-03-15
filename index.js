1ad1d269e49e5d94ec8ef5a2ce545707e5f69d04
Damiangc99 <99183982+Damiangc99@users.noreply.github.com>
2022-03-15 11:56:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const express = require("express")
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get("/",(req,res)=>{
-    console.log("Requested / route");
+app.use("/",express.static('public'));
+
+app.get("/faces",(req,res)=>{
+    console.log("Requested / faces");
     res.send("<html><body><h1>"+cool()+"</h1></body></html>")
 })
 
