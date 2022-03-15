4c64bff227252d600b39b50249268084e10084f5
Damiangc99 <99183982+Damiangc99@users.noreply.github.com>
2022-03-15 11:42:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const cool = require("cool-ascii-faces");
+const express = require("express")
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/",(req,res)=>{
+    console.log("Requested / route");
+    res.send("<html><body><h1>"+cool()+"</h1></body></html>")
+})
+
+app.listen(port, () => {
+    console.log('server ready at port ${port}');
+})
