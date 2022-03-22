31c881565a18e7f5994efb7e061cfe3af25a5e1b
AngelSM11 <82569640+AngelSM11@users.noreply.github.com>
2022-03-22 12:58:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,38 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 8080;
 
 const BASE_API_URL = "/api/v1";
-const api_doc = "https://docs.google.com/document/d/1my9fboOZnBnGUS0nW65O653r2NRP5Ol4Q2oFTw9MQpU/";
+//DAMIAN
+const api_doc_DGC= "https://docs.google.com/document/d/1my9fboOZnBnGUS0nW65O653r2NRP5Ol4Q2oFTw9MQpU/";
+//ANGEL
+const api_doc_ASM = "https://docs.google.com/document/d/1_LHw0xnOvLafWNtNXzWkdP922CPShLoMHLHixCJYO9A/edit";
+
+//ANGEL
+
+var esco=[
+    {
+        country: "eeuu",
+        year: 2019,
+        esco_tot: 102.5
+    },
+    {
+        country: "spain",
+        year: 2019,
+        esco_tot: 100.7
+    },
+    {
+        country: "argentina",
+        year: 2019,
+        esco_tot: 116.5
+    },
+    {
+        country: "madagascar",
+        year: 2019,
+        esco_tot: 102.4
+    },
+ 
+];
+
+//DAMIAN
 
 var economies=[
     {
@@ -35,8 +66,24 @@ var economies=[
  
 ];
 
+//ANGEL
+
+app.get(BASE_API_URL+"/esco/docs",(req,res)=>{
+    res.redirect(api_doc_ASM);
+});    
+
+app.get(BASE_API_URL+"/esco",(req,res)=>{
+    res.send(JSON.stringify(esco,null,2));
+});
+
+app.post(BASE_API_URL+"/esco",(req,res)=>{
+    contacts.push(req.body);
+    res.sendStatus(201,"CREATED");
+   
+//DAMIAN
+
 app.get(BASE_API_URL+"/economies/docs",(req,res)=>{
-    res.redirect(api_doc);
+    res.redirect(api_doc_DGC);
 });    
 
 app.get(BASE_API_URL+"/economies",(req,res)=>{
