ba01a6d5f796b1c18034431e561c77f9ed11cf99
AngelSM11 <82569640+AngelSM11@users.noreply.github.com>
2022-04-02 11:10:35 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const cool = require("cool-ascii-faces");
+const express = require("express");
+const bodyParser = require("body-parser")
+const app = express();
+app.use(bodyParser.json());
+const port = process.env.PORT || 8080;
+
+const cancerdeaths_stats_API = require("./src/back/cancerdeaths-stats.js");
+const pneumonia_stats_API = require("./src/back/pneumonia-stats.js");
+const airpollution_stats_API = require("./src/back/air-pollution-stats.js");
+
+
+const BASE_API_URL = "/api/v1/";
+
+
+
+cancerdeaths_stats_API.register(app);
+pneumonia_stats_API.register(app);
+airpollution_stats_API.register(app);
+
+app.use("/", express.static(`public`))
+
+app.get("/cool", (req,res)=>{
+    console.log("Requested /cool route");
+    res.send("<html><body><h1>" + cool()+ "</h1></body></html>")
+});
+
+app.listen(port, ()=>{
+    console.log(`Server ready at port ${port}`);
+});
