bb5dde34d5617da4ebb03a22b171db3ac1d3c964
= <damiangc99@gmail.com>
2022-04-11 12:50:21 +0200

    nuevos cambios

--- index.js
+++ index.js
@@ -18,10 +18,6 @@ economies_stats_API.register(app);
 
 app.use("/", express.static(`public`));
 
-app.get("/cool", (req,res)=>{
-    console.log("Requested /cool route");
-    res.send("<html><body><h1>" + cool()+ "</h1></body></html>")
-});
 
 app.listen(port, ()=>{
     console.log(`Server ready at port ${port}`);
