45c2c871628f94c83bdd9b0f083c83c333579f3f
= <damiangc99@gmail.com>
2022-04-17 12:54:03 +0200

    public separado

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 8080;
 
 const economies_stats_API = require("./src/back/economies-stats.js");
-
+const esco_stats_API = require("./src/back/esco-stats.js");
+const expo_stats_API = require("./src/back/expo-stats.js");
 
 
 const BASE_API_URL = "/api/v1/";
@@ -13,6 +14,8 @@ const BASE_API_URL = "/api/v1/";
 
 
 economies_stats_API.register(app);
+esco_stats_API.register(app);
+expo_stats_API.register(app);
 
 
 app.use("/api/v1/svelteEconomies", express.static("./frontEconomies/public"));
