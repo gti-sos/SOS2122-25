b2a235f86792bcea980e433c08e4eaca9218a1ea
AngelSM11 <82569640+AngelSM11@users.noreply.github.com>
2022-04-24 20:05:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const port = process.env.PORT || 8080;
 const economies_stats_API = require("./src/back/economies-stats.js");
 const esco_stats_API = require("./src/back/esco-stats.js");
 const expo_stats_API = require("./src/back/expo-stats.js");
-
+const Datastore = require('nedb');
 
 const BASE_API_URL = "/api/v1/";
 
-
+db_esco_stats = new Datastore();
 
 economies_stats_API.register(app);
-esco_stats_API.register(app);
+esco_stats_API.register(app,db_esco_stats);
 expo_stats_API.register(app);
 
 app.use("/api/v1/svelteExpo", express.static("./src/front/frontExpo/public"));
 app.use("/api/v1/svelteEconomies", express.static("./src/front/frontEconomies/public"));
-app.use("/api/v1/svelteEsco", express.static("./src/front/frontEsco/public"));
+
 app.use("/",express.static("./public"));
 
 
