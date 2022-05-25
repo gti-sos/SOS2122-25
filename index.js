457836692a341f91c4c86c5c2e05ae3fa54a8764
= <damiangc99@gmail.com>
2022-05-25 19:03:35 +0200

    integracion1 terminada

--- index.js
+++ index.js
@@ -14,36 +14,12 @@ const esco_stats_API = require("./src/back/esco-stats.js");
 const expo_stats_API = require("./src/back/expo-stats.js");
 const Datastore = require('nedb');
 
-/*
-var jwt = require('jsonwebtoken')
-
-user=1234;
-
-function generateToken(user) {
-  var u = {
-   username: user.username,
-   id: user.id
-  }
-  return token = jwt.sign(u, 1234, {
-     expiresIn: 60 * 60 * 24 // expires in 24 hours
-  })
-}
-*/
-
 db_economies_stats = new Datastore();
 db_esco_stats = new Datastore();
 db_expo_stats = new Datastore();
 
 
-//Proxy economies
-var paths='/remoteAPI';
-var apiServerHost = 'https://sos2122-20.herokuapp.com/api/v1/fertilizers-stats';
 
-app.use(paths, function(req, res) {
-    var url = apiServerHost + req.url;
-    console.log('piped: ' + req.url);
-    req.pipe(request(url)).pipe(res);
-});
   
 
 
@@ -62,31 +38,5 @@ app.listen(port, ()=>{
     console.log(`Server ready at port ${port}`);
 });
 
-/*
-app.use('/',function(req, res, next) {
-    generateToken(user)
-    var token = req.headers['authorization']
-    if (!token) {
-      res.status(401).send({
-        ok: false,
-        message: 'Toket inválido'
-      })
-    }
-  
-    token = token.replace('Bearer ', '')
-  
-    jwt.verify(token, 1234, function(err, token) {
-      if (err) {
-        return res.status(401).send({
-          ok: false,
-          message: 'Toket inválido'
-        });
-      } else {
-        req.token = token
-        next()
-      }
-    });
-  });
-*/
 
 
